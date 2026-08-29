f3101ea86b16848cfda8feaa508c979c7a64d128
allykelvin987-spec <allykelvin987@gmail.com>
2026-08-29 15:09:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
+const express = require("express");
 
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+module.exports = app;
